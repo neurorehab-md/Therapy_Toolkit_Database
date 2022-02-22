5202d6875bd34e2e94faf4ca0dfda41ba72c1972
shmueldabomb441 <60489781+shmueldabomb441@users.noreply.github.com>
2022-02-22 12:31:41 -0500

    Update Download java.bat

--- Setup files/Download java.bat
+++ Setup files/Download java.bat
@@ -1,1 +1,2 @@
 start https://github.com/ojdkbuild/ojdkbuild/releases/download/java-1.8.0-openjdk-1.8.0.242-1.b08/java-1.8.0-openjdk-1.8.0.242-1.b08.ojdkbuild.windows.x86_64.msi
+::local folder without adding to PATH: https://download.java.net/java/GA/jdk11/9/GPL/openjdk-11.0.2_windows-x64_bin.zip
